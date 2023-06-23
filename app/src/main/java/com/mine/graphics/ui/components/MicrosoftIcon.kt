4afaa755f9f26ae38e14e6d39555fbdcbc34d544
carrelle <esthcarelle@gmail.com>
2023-06-23 10:54:08 +0200

    Removed unused functions and unused imports

--- app/src/main/java/com/mine/graphics/ui/components/MicrosoftIcon.kt
+++ app/src/main/java/com/mine/graphics/ui/components/MicrosoftIcon.kt
@@ -2,16 +2,12 @@ package com.mine.graphics.ui.components
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.ClipOp
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.graphics.drawscope.clipPath
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -20,20 +16,6 @@ fun MicrosoftIcon() {
     Canvas(modifier = Modifier.size(100.dp)) {
         val width = this.size.width
         val height = this.size.height
-
-        val path = Path()
-        path.moveTo(width / 2, height / 2)
-        path.lineTo(width + 5, height * .05f)
-        path.lineTo(width + 20, height * 0.45f)
-
-        clipPath(path = path, clipOp = ClipOp.Difference) {
-            drawCircle(
-                color = Color.Transparent,
-                radius = 100f,
-                style = Stroke(40f, cap = StrokeCap.Round)
-            )
-            drawPath(path = path, color = Color.Transparent)
-        }
 
         drawRect(
             color = Color(0xFFF25022),
@@ -63,5 +45,7 @@ fun MicrosoftIcon() {
 @Composable
 @Preview
 fun MicrosoftPreview() {
-    MicrosoftIcon()
+    Surface(color = Color.White) {
+        MicrosoftIcon()
+    }
 }
